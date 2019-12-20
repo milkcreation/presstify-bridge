ce690bffcf27d2cd588b6f085ea395f3a064c6f6
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-12-20 17:25:07 +0000

    bridge -- branch 2.0 -- correctifs tinymce + Assets Column\PostThumbnail

--- bridge/branches/2.0/Bridge.php
+++ bridge/branches/2.0/Bridge.php
@@ -10,7 +10,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc Bridge PresstifyFramework.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.239
+ * @version 2.0.240
  * @copyright Milkcreation
  */
 class Bridge extends Container
