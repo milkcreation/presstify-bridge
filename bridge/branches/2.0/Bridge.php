8e2dbeb75947976407f4248c1b38d299d6718ae7
5upp0r7 <5upp0r7@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-02-13 15:44:39 +0000

    bridge -- branch 2.0 -- SelectJs Observer + Modal réécriture partie Ajax phase #1 + Field & Partial get instance unique + Wordpress Route & RouteGroup contracts

--- bridge/branches/2.0/Bridge.php
+++ bridge/branches/2.0/Bridge.php
@@ -10,7 +10,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc Bridge PresstifyFramework.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy\Plugins\Bridge
- * @version 2.0.255
+ * @version 2.0.256
  * @copyright Milkcreation
  */
 class Bridge extends Container
